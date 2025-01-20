f92d70d3db35db136166afd14a683f17e21e514d
osteensouthpaw <osteensouthpaw@gmail.com>
2025-01-20 16:43:26 +0300

    fixed a bug on the jpql query

--- src/main/java/com/omega/jobportal/user/UserRepository.java
+++ src/main/java/com/omega/jobportal/user/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     @Query("""
-   SELECT u FROM AppUser u WHERE u.email = ?
+   SELECT u FROM AppUser u WHERE u.email = :email
 """)
     Optional<AppUser> findUserByEmail(String email);
 }
