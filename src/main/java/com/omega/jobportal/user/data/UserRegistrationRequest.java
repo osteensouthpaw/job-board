a50439c901df4c607940aac739c34a3513900a2b
osteensouthpaw <osteensouthpaw@gmail.com>
2025-01-23 20:47:57 +0300

    add validation to fields

--- src/main/java/com/omega/jobportal/user/data/UserRegistrationRequest.java
+++ src/main/java/com/omega/jobportal/user/data/UserRegistrationRequest.java
@@ -3,27 +3,33 @@ package com.omega.jobportal.user.data;
 import com.omega.jobportal.user.Gender;
 import com.omega.jobportal.user.UserType;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserRegistrationRequest(
-        @NotNull(message = "first name is required")
+        @NotBlank(message = "first name is required")
         String firstName,
 
-        @NotNull(message = "last name is required")
+        @NotBlank(message = "last name is required")
         String lastName,
 
         @NotNull(message = "userType is required")
         UserType userType,
 
         @Email
-        @NotNull(message = "email is required")
+        @NotBlank(message = "email is required")
         String email,
 
         String phone,
+
         @Size(min = 8)
+        @NotBlank
         String password,
+
         Gender gender,
+
+
         String imageUrl
 ) {
 }
