3f5f0c33d3f38be3db381af11dbdde0e65794f77
osteensouthpaw <osteensouthpaw@gmail.com>
2025-01-27 21:11:52 +0300

    implement update jobseeker education

--- src/main/java/com/omega/jobportal/jobSeekerProfile/dtoMapper/EducationDtoMapper.java
+++ src/main/java/com/omega/jobportal/jobSeekerProfile/dtoMapper/EducationDtoMapper.java
@@ -1,5 +1,6 @@
 package com.omega.jobportal.jobSeekerProfile.dtoMapper;
 
+import com.omega.jobportal.jobSeekerProfile.data.EducationRequest;
 import com.omega.jobportal.jobSeekerProfile.data.EducationResponse;
 import com.omega.jobportal.jobSeekerProfile.education.Education;
 import org.springframework.stereotype.Component;
@@ -20,5 +21,16 @@ public class EducationDtoMapper implements Function<Education, EducationResponse
                 education.getEndDate(),
                 education.getCertificateUrl()
         );
+    }
+
+    //update education: I used to the same dto for request since they share all the same details
+    public Education apply(EducationRequest request, Education education) {
+        education.setEducationLevel(request.educationLevel());
+        education.setFieldOfStudy(request.fieldOfStudy());
+        education.setInstitution(request.institution());
+        education.setStartDate(request.startDate());
+        education.setEndDate(request.endDate());
+        education.setCertificateUrl(request.certificateUrl());
+        return education;
     }
 }
