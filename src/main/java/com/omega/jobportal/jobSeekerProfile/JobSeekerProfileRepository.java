2d798f36b713b1408455a0d49826010edb099499
osteensouthpaw <osteensouthpaw@gmail.com>
2025-01-27 06:14:03 +0300

    implement view profile section

--- src/main/java/com/omega/jobportal/jobSeekerProfile/JobSeekerProfileRepository.java
+++ src/main/java/com/omega/jobportal/jobSeekerProfile/JobSeekerProfileRepository.java
@@ -1,6 +1,17 @@
 package com.omega.jobportal.jobSeekerProfile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Long> {
+
+    @Query(
+            """
+                        SELECT js FROM JobSeekerProfile js
+                        WHERE js.jobSeeker.id = :jobSeekerId
+                    """
+    )
+    Optional<JobSeekerProfile> findJobSeekerProfileByJobSeekerId(Long jobSeekerId);
 }
