9e9ae4558292db3c55849662b0d22a9e000dac01
osteensouthpaw <osteensouthpaw@gmail.com>
2025-01-28 09:19:40 +0300

    implement update experience functionality

--- src/main/java/com/omega/jobportal/jobSeekerProfile/experience/ExperienceService.java
+++ src/main/java/com/omega/jobportal/jobSeekerProfile/experience/ExperienceService.java
@@ -1,6 +1,7 @@
 package com.omega.jobportal.jobSeekerProfile.experience;
 
 import com.omega.jobportal.auth.AuthenticationService;
+import com.omega.jobportal.exception.ApiException;
 import com.omega.jobportal.jobSeekerProfile.JobSeekerProfile;
 import com.omega.jobportal.jobSeekerProfile.JobSeekerProfileService;
 import com.omega.jobportal.jobSeekerProfile.data.ExperienceRequest;
@@ -8,7 +9,10 @@ import com.omega.jobportal.jobSeekerProfile.data.ExperienceResponse;
 import com.omega.jobportal.jobSeekerProfile.dtoMapper.ExperienceDtoMapper;
 import com.omega.jobportal.user.AppUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +28,19 @@ public class ExperienceService {
         Experience experience = new Experience(request, jobSeekerProfile);
         experience = experienceRepository.save(experience);
         return experienceDtoMapper.apply(experience);
+    }
+
+    public ExperienceResponse updateExperience(ExperienceRequest request, Long id) {
+        AppUser loggedInUser = authenticationService.getSession();
+        return experienceRepository.findById(id)
+                .map(experience -> {
+                    boolean isExperienceOwner = Objects.equals(experience.getJobSeekerProfile().getJobSeeker().getId(), loggedInUser.getId());
+                    if (!isExperienceOwner)
+                        throw new ApiException("you can only edit your own experience", HttpStatus.FORBIDDEN);
+                    Experience updatedExperience = experienceDtoMapper.apply(request, experience);
+                    updatedExperience = experienceRepository.save(updatedExperience);
+                    return experienceDtoMapper.apply(updatedExperience);
+                })
+                .orElseThrow(() -> new ApiException("experience detail not found", HttpStatus.NOT_FOUND));
     }
 }
