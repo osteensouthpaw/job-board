1394108937947d9c4cd5efb4a2632a775ff3c68d
osteensouthpaw <osteensouthpaw@gmail.com>
2025-01-29 15:25:41 +0300

    implement update skill set functionality for job seekers

--- src/main/java/com/omega/jobportal/jobSeekerProfile/skills/SkillSetController.java
+++ src/main/java/com/omega/jobportal/jobSeekerProfile/skills/SkillSetController.java
@@ -4,10 +4,7 @@ import com.omega.jobportal.jobSeekerProfile.data.SkillSetRequest;
 import com.omega.jobportal.jobSeekerProfile.data.SkillSetResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +15,12 @@ public class SkillSetController {
     @PostMapping
     public ResponseEntity<SkillSetResponse> saveSkillSet(@RequestBody SkillSetRequest request) {
         SkillSetResponse skillSetResponse = skillSetService.saveSkillSet(request);
+        return ResponseEntity.ok(skillSetResponse);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<SkillSetResponse> updateSkillSet(@RequestBody SkillSetRequest request, @PathVariable Long id) {
+        SkillSetResponse skillSetResponse = skillSetService.updateSkillSet(request, id);
         return ResponseEntity.ok(skillSetResponse);
     }
 }
