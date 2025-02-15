bfdba2213024371dd33db0af9e2f3bc6c8fc55f1
osteensouthpaw <osteensouthpaw@gmail.com>
2025-02-15 13:23:57 +0300

    configure oauth2 login

--- src/main/java/com/omega/jobportal/config/SecurityConfig.java
+++ src/main/java/com/omega/jobportal/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,10 +27,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request
-                .requestMatchers("/api/v1/job-posts", "/api/v1/job-posts").hasAnyRole("RECRUITER", "JOB_SEEKER")
-                .anyRequest().permitAll());
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable);
+
+        http.oauth2Login(Customizer.withDefaults());
+        http.formLogin(Customizer.withDefaults());
+        http.exceptionHandling(customizer -> {
+            customizer.authenticationEntryPoint(
+                    (request, response, authException) -> {
+                        response.sendError(401, "Unauthorized");
+                    });
+        });
 
         return http.build();
     }
