81281113e54ba802cd68080a6eb1d7063967756b
osteensouthpaw <osteensouthpaw@gmail.com>
2025-02-25 15:32:21 +0300

    refactor application configuration files

--- src/main/java/com/omega/jobportal/JobPortalApplication.java
+++ src/main/java/com/omega/jobportal/JobPortalApplication.java
@@ -2,10 +2,13 @@ package com.omega.jobportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+
 @EnableAsync(proxyTargetClass = true)
+@EnableJpaAuditing
+@SpringBootApplication
 public class JobPortalApplication {
 
     public static void main(String[] args) {
