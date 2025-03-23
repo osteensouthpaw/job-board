443159ed3bab632fdbed26329b8f212689726d35
osteensouthpaw <osteensouthpaw@gmail.com>
2025-03-23 06:25:46 +0300

    implement filtering by min and max salary

--- backend/src/main/java/com/omega/jobportal/jobPost/JobPostSpecificationBuilder.java
+++ backend/src/main/java/com/omega/jobportal/jobPost/JobPostSpecificationBuilder.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public class JobPostSpecificationBuilder {
     public static Specification<JobPost> filterJobs(JobPostFilterQuery filterQuery) {
-        return (root, query, criteriaBuilder) -> {
+        return (root, _, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
             Optional.ofNullable(filterQuery.jobType())
                     .ifPresent(jobType -> predicates.add(criteriaBuilder.equal(root.get("jobType"), jobType)));
@@ -23,6 +23,12 @@ public class JobPostSpecificationBuilder {
 
             Optional.ofNullable(filterQuery.experienceLevel())
                     .ifPresent(experienceLevel -> predicates.add(criteriaBuilder.equal(root.get("experienceLevel"), experienceLevel)));
+
+            Optional.ofNullable(filterQuery.minSalary())
+                    .ifPresent(minSalary -> predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("salary"), minSalary)));
+
+            Optional.ofNullable(filterQuery.maxSalary())
+                    .ifPresent(maxSalary -> predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("salary"), maxSalary)));
 
             Optional.ofNullable(filterQuery.datePosted())
                     .ifPresent(datePosted -> predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), datePosted)));
--- backend/src/main/java/com/omega/jobportal/jobPost/data/JobPostFilterQuery.java
+++ backend/src/main/java/com/omega/jobportal/jobPost/data/JobPostFilterQuery.java
@@ -4,12 +4,15 @@ import com.omega.jobportal.jobPost.enumerations.ExperienceLevel;
 import com.omega.jobportal.jobPost.enumerations.JobType;
 import com.omega.jobportal.jobPost.enumerations.WorkMode;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record JobPostFilterQuery(
         JobType jobType,
         String location,
         WorkMode workMode,
+        BigDecimal minSalary,
+        BigDecimal maxSalary,
         ExperienceLevel experienceLevel,
         LocalDateTime datePosted
 ) {
