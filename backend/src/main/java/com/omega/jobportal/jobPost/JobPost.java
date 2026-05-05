65800f54994d2cb996ab5012568e4236e0d7bd63
osteensouthpaw <osteensouthpaw@gmail.com>
2026-05-05 06:15:09 +0300

    change description type form varchar to TEXT

--- backend/src/main/java/com/omega/jobportal/jobPost/JobPost.java
+++ backend/src/main/java/com/omega/jobportal/jobPost/JobPost.java
@@ -50,7 +50,7 @@ public class JobPost {
     @FullTextField
     private String jobTitle;
 
-    @Column(length = 1000)
+    @Column(columnDefinition = "TEXT")
     @FullTextField
     private String description;
 
